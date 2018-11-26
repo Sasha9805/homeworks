589c9e94f7758ed0b41d39584078862804626ef5
Sasha9805 <44747527+Sasha9805@users.noreply.github.com>
2018-11-27 00:03:06 +0200

    Add files via upload
    Добавлено задание на баллы 7-1. Типы данных JS

---
+++ points-7-1/script.js
@@ -0,0 +1,7 @@
+console.log("undefined " + typeof udefined);
+console.log("number 1024 " + typeof 1024);
+console.log("bool false " + typeof false);
+console.log("string '1024' " + typeof "1024");
+console.log("Object " + typeof {});
+console.log("Null как object!! " + typeof null);
+console.log("Функция как function!! " + typeof function(){});
